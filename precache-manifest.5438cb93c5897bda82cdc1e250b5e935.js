fa8fc19dd15a4e5488b89dfa25379ee911427d65
sora sorata <sora2ta@gmail.com>
2019-01-15 16:14:36 +0800

    first commit

---
+++ precache-manifest.5438cb93c5897bda82cdc1e250b5e935.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2c9098f7957a4fc9b226",
+    "url": "/static/js/main.2c9098f7.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "2c9098f7957a4fc9b226",
+    "url": "/static/css/main.95951d96.chunk.css"
+  },
+  {
+    "revision": "2f58d5f80149cb251feb05ca2b895b77",
+    "url": "/index.html"
+  }
+];
